249e104ffc17a6313de62c91e081606410331861
FATx64 <73746785+FATx64@users.noreply.github.com>
2021-11-01 14:13:34 +0700

    Create if( 1 conditions).c

---
+++ if( 1 conditions).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+  
+  int a;
+
+  printf("contoh kondisi if satu kasus \n");
+  printf("masukkan nilai a : ");
+  scanf("%d",&a);
+
+  if (a >=6 ){
+    printf("Nilai a positif %d \n",a);
+  }
+
+  return 0;
+
+
+}
