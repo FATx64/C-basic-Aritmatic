ef7b7b18c9db06b467dca9f52e5419fbb328d67d
FATx64 <73746785+FATx64@users.noreply.github.com>
2021-11-08 13:12:19 +0700

    Create If (2conditions).c

---
+++ If (2conditions).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+  
+  int a;
+
+  printf("contoh kondisi if dua kasus \n");
+  printf("masukkan nilai a : ");
+  scanf("%d",&a);
+
+  if (a >=5 ){
+    printf("Nilai a lebih besar %d \n",a);
+  }
+
+  else{
+    printf("Nilai a lebih kecil %d \n",a);
+  }
+
+  return 0;
+
+
+}
